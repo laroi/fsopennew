e1334d583eddc4d085612d2039ff0c335c1440d4
Akbar <akbarali1klr@gmail.com>
2019-11-14 22:52:13 +0530

    3.2: Phonebook backend step2

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+}
+ ]
+
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+app.get('/info', (req, res) => {
+    res.send(`<p>phonebook has info for ${persons.length} people </p> <p>${new Date()}</p>`)
+})
+app.get('/api/persons/:id', (req, res) => {
+    const person = persons.find(x=>x.id===parseInt(req.params.id, 10));
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).send();
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
