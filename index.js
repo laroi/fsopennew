a6e0c340f98b3e94f8ffcb4719f1b9ddd016fe12
Akbar <akbarali1klr@gmail.com>
2019-11-23 00:12:38 +0530

    3.13: Phonebook database, step1 && 3.14: Phonebook database, step2

--- index.js
+++ index.js
@@ -3,58 +3,47 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config();
+const Entry = require('./models/entry')
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static('build'));
 morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-}
- ]
+
 
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+    Entry.find({}).then(result => {      
+        res.json(result.map(x=>x.toJSON()))
+    })
+
 })
 app.get('/info', (req, res) => {
-    res.send(`<p>phonebook has info for ${persons.length} people </p> <p>${new Date()}</p>`)
+        Entry.count({}).then(result => {      
+
+    res.send(`<p>phonebook has info for ${result} people </p> <p>${new Date()}</p>`)
+        })
 })
 app.get('/api/persons/:id', (req, res) => {
-    const person = persons.find(x=>x.id===parseInt(req.params.id, 10));
-    if (person) {
-        res.json(person);
-    } else {
-        res.status(404).send();
-    }
+    Entry.findOne({_id:req.params.id}).then(result => {
+        if (result) {
+            res.json(result);
+        } else {
+            res.status(404).send();
+        }
+
+    })
 })
 app.delete('/api/persons/:id', (req, res) => {
-    const index = persons.findIndex(x=>x.id===parseInt(req.params.id, 10));
-    console.log(persons[index]);
-    if (index > 0) {
-        persons.splice(index, 1);
-        res.status(204).send();
-    } else {
-        res.status(404).send();
-    }
+    Entry.remove({_id:req.params.id}).then(result => {
+        if (result) {
+            res.status(204).send();
+        } else {
+            res.status(404).send();
+        }
+    })
+
 })
 app.post('/api/persons', (req, res)=> {
     const number = req.body.number;
@@ -63,13 +52,11 @@ app.post('/api/persons', (req, res)=> {
         res.status(400).send({error: "number is not provided"})
         return;
     }
-    exists = persons.findIndex(x=>x.number===number)
-    if (exists > -1) {
-        res.status(409).send({error: 'number already exists'})
-        return;
-    }
-    persons.push({id: Math.floor(Math.random() * 1000), name: name, number: number});
-    res.status(201).send();
+    const phoneObj = new Entry({name: name, phoneNumber:number});
+    phoneObj.save()
+    .then(x=> {
+        res.status(201).send();
+    })
 })
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ models/entry.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
+mongoose.connect(url, { useNewUrlParser: true })
+  .then(result => {    console.log('connected to MongoDB')  })  .catch((error) => {    console.log('error connecting to MongoDB:', error.message)  })
+const entrySchema = new mongoose.Schema({
+  id: String,
+  name: String,
+  phoneNumber: String,
+})
+
+entrySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
+module.exports = mongoose.model('Entry', entrySchema)
