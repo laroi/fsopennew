6f728a8817b1606e736fbd3cbf8374981ae68b99
Akbar <akbarali1klr@gmail.com>
2019-11-23 00:48:44 +0530

    3.15: Phonebook database, step3 & 3.16: Phonebook database, step3 & 3.17*: Phonebook database, step4 & 3.18*: Phonebook database step5

--- index.js
+++ index.js
@@ -13,29 +13,29 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :t
 
 
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
     Entry.find({}).then(result => {      
         res.json(result.map(x=>x.toJSON()))
     })
-
+    .catch(error => next(error))
 })
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
         Entry.count({}).then(result => {      
 
     res.send(`<p>phonebook has info for ${result} people </p> <p>${new Date()}</p>`)
         })
 })
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Entry.findOne({_id:req.params.id}).then(result => {
         if (result) {
             res.json(result);
         } else {
             res.status(404).send();
         }
-
     })
+    .catch(error => next(error))
 })
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     Entry.remove({_id:req.params.id}).then(result => {
         if (result) {
             res.status(204).send();
@@ -43,21 +43,56 @@ app.delete('/api/persons/:id', (req, res) => {
             res.status(404).send();
         }
     })
+    .catch(error => next(error))
 
 })
-app.post('/api/persons', (req, res)=> {
+app.post('/api/persons', (req, res, next)=> {
     const number = req.body.number;
     const name = req.body.name;
     if (!number) {
         res.status(400).send({error: "number is not provided"})
         return;
     }
-    const phoneObj = new Entry({name: name, phoneNumber:number});
-    phoneObj.save()
+    Entry.findOne({name:name})
     .then(x=> {
-        res.status(201).send();
+        if (x) {
+            res.status(409).send(x.toJSON())
+            return
+        }
+        const phoneObj = new Entry({name: name, phoneNumber:number});
+        phoneObj.save()
+        .then(x=> {
+            res.status(201).send();
+        })
+        .catch(error => next(error))
     })
+    .catch(error => next(error))
 })
+app.post('/api/persons/:id', (req, res, next)=> {
+    const number = req.body.number;
+    const name = req.body.name;
+    if (!number) {
+        res.status(400).send({error: "number is not provided"})
+        return;
+    }
+    Entry.findByIdAndUpdate(req.params.id, {name:name, phoneNumber:number}, {new: true})
+    .then(x=> {
+        res.json(x.toJSON());
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler);
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
