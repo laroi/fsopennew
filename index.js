eba2fb1aab3dedfbb03453721802f61cdf4f466c
Akbar <akbarali1klr@gmail.com>
2019-11-24 20:39:06 +0530

    3.21 Deploying the database backend to production & 3.22: Lint configuration

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
-const Entry = require('./models/entry')
-app.use(cors())
-app.use(bodyParser.json())
+const Entry = require('./models/entry');
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static('build'));
-morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+morgan.token('type', function (req) { return JSON.stringify(req.body); });
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'));
 
 
 
 app.get('/api/persons', (req, res, next) => {
     Entry.find({}).then(result => {      
-        res.json(result.map(x=>x.toJSON()))
+        res.json(result.map(x=>x.toJSON()));
     })
-    .catch(error => next(error))
-})
+        .catch(error => next(error));
+});
 app.get('/info', (req, res, next) => {
-        Entry.count({}).then(result => {      
-
-    res.send(`<p>phonebook has info for ${result} people </p> <p>${new Date()}</p>`)
-        })
-})
+    Entry.count({}).then(result => {      
+        res.send(`<p>phonebook has info for ${result} people </p> <p>${new Date()}</p>`);
+    })
+        .catch(error => next(error));
+});
 app.get('/api/persons/:id', (req, res, next) => {
     Entry.findOne({_id:req.params.id}).then(result => {
         if (result) {
@@ -33,8 +33,8 @@ app.get('/api/persons/:id', (req, res, next) => {
             res.status(404).send();
         }
     })
-    .catch(error => next(error))
-})
+        .catch(error => next(error));
+});
 app.delete('/api/persons/:id', (req, res, next) => {
 
     Entry.findByIdAndRemove(req.params.id).then(result => {
@@ -44,53 +44,52 @@ app.delete('/api/persons/:id', (req, res, next) => {
             res.status(404).send();
         }
     })
-    .catch(error => next(error))
+        .catch(error => next(error));
 
-})
+});
 app.post('/api/persons', (req, res, next)=> {
     const number = req.body.phoneNumber;
     const name = req.body.name;
     if (!number) {
-        res.status(400).send({error: "number is not provided"})
+        res.status(400).send({error: 'number is not provided'});
         return;
     }
 
     const phoneObj = new Entry({name: name, phoneNumber:number});
     phoneObj.save()
-    .then(x=> {
-        console.log(x.toJSON());
-        res.status(201).send(x.toJSON());
-    })
-    .catch(error => next(error))
-})
+        .then(x=> {
+            console.log(x.toJSON());
+            res.status(201).send(x.toJSON());
+        })
+        .catch(error => next(error));
+});
 app.put('/api/persons/:id', (req, res, next)=> {
     const number = req.body.number;
     const name = req.body.name;
     if (!number) {
-        res.status(400).send({error: "number is not provided"})
+        res.status(400).send({error: 'number is not provided'});
         return;
     }
     Entry.findByIdAndUpdate(req.params.id, {name:name, phoneNumber:number}, {new: true})
-    .then(x=> {
-        res.json(x.toJSON());
-    })
-    .catch(error => next(error))
-})
+        .then(x=> {
+            res.json(x.toJSON());
+        })
+        .catch(error => next(error));
+});
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.name)
-
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-    else if (error.name === 'ValidationError') {  
-        return response.status(400).json({ error: error.message })
+    console.error(error.name);
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' });
     }
-  next(error)
-}
+    else if (error.name === 'ValidationError') {  
+        return response.status(400).json({ error: error.message });
+    }
+    next(error);
+};
 
 app.use(errorHandler);
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
--- models/entry.js
+++ models/entry.js
@@ -1,21 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
+const url = process.env.MONGODB_URI;
+console.log('connecting to', url);
 mongoose.connect(url, { useNewUrlParser: true })
-  .then(result => {    console.log('connected to MongoDB')  })  .catch((error) => {    console.log('error connecting to MongoDB:', error.message)  })
+    .then(() => {    console.log('connected to MongoDB');  })  .catch((error) => {    console.log('error connecting to MongoDB:', error.message);  });
 const entrySchema = new mongoose.Schema({ 
-    name: {type:  String, required: true},
-  phoneNumber: {type: String, required: true, minlength:8, unique: true},
-})
+    name: {type:  String, required: true, unique: true},
+    phoneNumber: {type: String, required: true, minlength:8, unique: true},
+});
 
 entrySchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
 entrySchema.plugin(uniqueValidator);
-
-module.exports = mongoose.model('Entry', entrySchema)
+module.exports = mongoose.model('Entry', entrySchema);
